5e1d780c1b660d56e71923522a61cd942321925e
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-09-21 13:52:33 +0200

    added dependencies

--- index.js
+++ index.js
@@ -3,171 +3,190 @@
 /* NOTES
 
 - access key (jwt) with privileges?
-	- read message
-	- redeem responses
-	- see responses
+    - read message
+    - redeem responses
+    - see responses
 - 
 
 */
 
 const express = require('express');
 const OBJY = require('objy');
+var shortid = require('shortid');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var moment = require("moment")
 
-OBJY.useClient('public');
-OBJY.useApp('kju');
+var app = express();
 
-OBJY.define({
-	name: "message",
-	pluralName: "mesages",
-	storage: {}, // MONGO
-	processor: {}, // BULLMQ
-})
-
-OBJY.define({
-	name: "callLog",
-	pluralName: "callLogs",
-	storage: {}, // MONGO
-})
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json({
+    limit: '300mb'
+}));
+app.use(cors());
+app.options('*', cors());
 
 var router = express.Router();
 
+OBJY.client('public');
+OBJY.app('kju');
+
+OBJY.define({
+    name: "message",
+    pluralName: "mesages",
+    //storage: {}, // MONGO
+    //processor: {}, // BULLMQ
+})
+
+OBJY.define({
+    name: "callLog",
+    pluralName: "callLogs",
+    //storage: {}, // MONGO
+})
+
+
+
 
 router.route('/creationToken')
-	
-	// get a token
-	.get(function(req, res) {
 
-	});
-
-router.route('/message')
-	
-	// create a message
+    // get a token
     .post(function(req, res) {
-
-    	/*
-			{
-				content: "",
-				//id: "(autofilled),
-				//ttl: "(autofilled)",
-				// payload: "(optional sturcture. e.g. image, description, ...)",
-				responses: [
-					{
-						//id: "(auto generated)",
-						title: "",
-						action: "(optional dsl)"
-					}
-				]"
-			}
-    	*/
-
-    	OBJY.message({
-    		name: content,
-    		_id: id,
-    		properties: {
-    			content: {
-    				type: "longText",
-    				value: req.body.content
-    			},
-    			ttl: {
-    				type: "number",
-    				value: req.body.ttl || 60000
-    			},
-    			responses:{
-    				type: "bag",
-    				properties: req.body.responses // TODO ARRAY TO OBJECT
-    			},
-    			superToken: { // full admin privileges for that message and corresponding responses
-    				type: "longText",
-    				value: "" // JWT
-    			},
-    			redeemToken: { // privilege for redeeming responses on that message
-    				type: "longText",
-    				value: "" // JWT
-    			},
-    			responsesToken: { // privilege for viewing responses for that message
-    				type: "longText",
-    				value: "" // JWT
-    			}
-    		}
-    	}).add(data => {
-    		res.json(data.properties)
-    	}, err => {
-    		res.status(400).json({err: "an error occured"})
-    	})
 
     });
 
-    
+router.route('/message')
+
+    // create a message
+    .post(function(req, res) {
+
+        /*
+            {
+                content: "",
+                //id: "(autofilled),
+                //ttl: "(autofilled)",
+                // payload: "(optional sturcture. e.g. image, description, ...)",
+                responses: [
+                    {
+                        //id: "(auto generated)",
+                        title: "",
+                        action: "(optional dsl)"
+                    }
+                ]"
+            }
+        */
+
+        OBJY.message({
+            name: req.body.content,
+            _id: shortid.generate(),
+            properties: {
+                content: {
+                    type: "longText",
+                    value: req.body.content
+                },
+                ttl: {
+                    type: "number",
+                    value: req.body.ttl || 60000
+                },
+                responses: {
+                    type: "bag",
+                    properties: req.body.responses // TODO ARRAY TO OBJECT
+                },
+                superToken: { // full admin privileges for that message and corresponding responses
+                    type: "longText",
+                    value: "" // JWT
+                },
+                redeemToken: { // privilege for redeeming responses on that message
+                    type: "longText",
+                    value: "" // JWT
+                },
+                responsesToken: { // privilege for viewing responses for that message
+                    type: "longText",
+                    value: "" // JWT
+                }
+            }
+        }).add(data => {
+            res.json(data.properties)
+        }, err => {
+            res.status(400).json({ err: "an error occured" })
+        })
+
+    });
+
+
 router.route('/message/:messageId')
-	
-	// get a single message
+
+    // get a single message
     .get(function(req, res) {
 
-    	OBJY.message(req.params.messageId).get((data) => {
-    		res.json(data);
-    	}, err => {
-    		res.status(404).json({err: "error getting message"})
-    	})
+        OBJY.message(req.params.messageId).get((data) => {
+            res.json(data);
+        }, err => {
+            console.log(err)
+            res.status(404).json({ err: "error getting message" })
+        })
     });
 
 router.route('/message/:messageId/responses')
-	
-	// get the responses for a message
+
+    // get the responses for a message
     .get(function(req, res) {
 
-    	OBJY.message(req.params.messageId).get((data) => {
-    		
-    		OBJY.callLogs({name: req.params.messageId}).get(data => {
-    			/*
-					{
-						resId: {
-							title: "original title",
-							responses: [{
-								_id: "(from db)",
-								time: "(from db: created)",
-								content: "(from db: response content)"
-							}]
-						}
-					}
-    			*/
-    		})
+        OBJY.message(req.params.messageId).get((data) => {
 
-    	}, err => {
-    		res.status(404).json({err: "error getting message"})
-    	})
+            OBJY.callLogs({ name: req.params.messageId }).get(data => {
+                /*
+                    {
+                        resId: {
+                            title: "original title",
+                            responses: [{
+                                _id: "(from db)",
+                                time: "(from db: created)",
+                                content: "(from db: response content)"
+                            }]
+                        }
+                    }
+                */
+            })
+
+        }, err => {
+            res.status(404).json({ err: "error getting message" })
+        })
     });
 
-   
+
 router.route('/message/:messageId/response/:responseId')
-	
-	 // redeem a response
+
+    // redeem a response
     .get(function(req, res) {
 
-    	OBJY.message(req.params.messageId).get((data) => {
-    		
-    		var res = data.properties.responses[req.params.responseId];
+        OBJY.message(req.params.messageId).get((data) => {
 
-    		if(!res)
-    			return res.status(404).json({err: "response not found"});
+            var res = data.properties.responses[req.params.responseId];
 
-    		// call action
-    		if(res.action)
-    			puzzle.parse(res.action);
+            if (!res)
+                return res.status(404).json({ err: "response not found" });
 
-    		OBJY.callLog({
-    			// ...
-    		}).add(data => {
+            // call action
+            if (res.action)
+                puzzle.parse(res.action);
 
-    		}, err => {
+            OBJY.callLog({
+                // ...
+            }).add(data => {
 
-    		})
+            }, err => {
 
-    	}, err => {
-    		res.status(404).json({err: "error getting message"})
-    	})
+            })
+
+        }, err => {
+            res.status(404).json({ err: "error getting message" })
+        })
 
     });
 
 
-
-
+app.listen(80);
+app.use('/api', router);
+console.log('server running');
